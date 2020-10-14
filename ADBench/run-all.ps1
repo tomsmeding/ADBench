925e1c124bc1cc7419165fc9e068937712abd0ae
Tom Smeding <tom.smeding@gmail.com>
2020-10-14 21:00:23 +0200

    Add Accelerate with 1 thread to run-all.ps1

--- ADBench/run-all.ps1
+++ ADBench/run-all.ps1
@@ -686,7 +686,8 @@ $default_tolerance = 1e-8
 # Separate Full|Split?
 # Separate GMM sizes?
 $tool_descriptors = @(
-    [Tool]::new("Accelerate", "bin", [ObjectiveType] "GMM", $true, $default_tolerance, true, $false)
+    [Tool]::new("Accelerate", "bin", [ObjectiveType] "GMM", $false, 0.0, $true, $false)
+    [Tool]::new("Accelerate1", "bin", [ObjectiveType] "GMM", $false, 0.0, $true, $false)
     #[Tool]::new("Adept", "bin", [ObjectiveType] "GMM, BA, Hand", $false, 0.0, $true, $false)
     #[Tool]::new("ADOLC", "bin", [ObjectiveType] "GMM, BA, Hand", $false, 0.0, $true, $false)
     #[Tool]::new("ADOLCEigen", "bin", [ObjectiveType] "Hand", $false, 0.0, $true, $false)
