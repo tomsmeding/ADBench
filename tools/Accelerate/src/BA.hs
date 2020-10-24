8c1a83b9678784f789ca0cd0cb284162eacce5ad
Tom Smeding <tom.smeding@gmail.com>
2020-10-24 17:20:22 +0200

    Initial BA code for Accelerate

---
+++ tools/Accelerate/src/BA.hs
@@ -0,0 +1,184 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+module BA (
+    baObjective, baObjectiveProgram,
+    baObjectiveJac, baObjectiveJacProgram,
+) where
+
+import Prelude ()
+import Control.DeepSeq (NFData)
+import Data.Array.Accelerate hiding (pi)
+import qualified Data.Array.Accelerate.Interpreter as I
+import qualified Data.Array.Accelerate.LLVM.Native as CPU
+
+import BAIO
+import Types
+
+
+camIndex :: Exp Camera -> Exp Int -> Exp FLT
+camIndex (Camera (T3 r1 r2 r3) (T3 c1 c2 c3) f (T2 x1 x2) (T2 k1 k2)) =
+    pick [r1, r2, r3, c1, c2, c3, f, x1, x2, k1, k2]
+  where
+    pick [x] _ = x
+    pick (x:xs) i = cond (i == 0) x (pick xs (i-1))
+    pick [] _ = error "pick: what"
+
+ptIndex :: Exp Pt3D -> Exp Int -> Exp FLT
+ptIndex (T3 x y z) i = cond (i == 0) x (cond (i == 1) y z)
+
+
+vecadd :: TLift t FLT => Exp t -> Exp t -> Exp t
+vecadd = tlift @_ @FLT (+)
+
+vecsub :: TLift t FLT => Exp t -> Exp t -> Exp t
+vecsub = tlift @_ @FLT (-)
+
+dot :: TLift t FLT => Exp t -> Exp t -> Exp FLT
+dot v w = treduce (+) (tlift @_ @FLT (*) v w)
+
+scale :: TLift t FLT => Exp FLT -> Exp t -> Exp t
+scale = tlift1 (*)
+
+cross :: Exp Pt3D -> Exp Pt3D -> Exp Pt3D
+cross (T3 a b c) (T3 x y z) = T3 (b*z - c*y) (c*x - a*z) (a*y - b*x)
+
+rodriguez :: Exp Pt3D -> Exp Pt3D -> Exp Pt3D
+rodriguez r@(T3 r1 r2 r3) x =
+    let theta = r1 * r1 + r2 * r2 + r3 * r3
+        v = T3 (r1 / theta) (r2 / theta) (r3 / theta)
+    in cond (theta == 0)
+            (x `vecadd` cross r x)
+            (scale (cos theta) x
+                `vecadd` scale (sin theta) (cross v x)
+                `vecadd` scale ((v `dot` x) * (1 - cos theta)) v)
+
+p2e :: Exp Pt3D -> Exp Pt2D
+p2e (T3 x1 x2 x3) = T2 (x1 / x3) (x2 / x3)
+
+distort :: Exp Pt2D -> Exp Pt2D -> Exp Pt2D
+distort (T2 k1 k2) u@(T2 u1 u2) =
+    let lensq = u1 * u1 + u2 * u2
+    in scale (1 + k1 * lensq + k2 * lensq * lensq) u
+
+project :: Exp Camera -> Exp Pt3D -> Exp Pt2D
+project (Camera r c f x0 kappa) x =
+    scale f (distort kappa (p2e (rodriguez r (x `vecsub` c)))) `vecadd` x0
+
+reprojerr :: Exp Camera -> Exp Pt3D -> Exp FLT -> Exp Pt2D -> Exp Pt2D
+reprojerr cam x w m = scale w (m `vecsub` project cam x)
+
+werr :: Exp FLT -> Exp FLT
+werr w = 1 - w * w
+
+objective :: Acc BAIn -> Acc BAFVal
+objective (BAIn cams pts ws feats obs) =
+    BAFVal (map (\(T2 i (Observation ci pi)) ->
+                     reprojerr (cams ! I1 ci) (pts ! I1 pi) (ws ! i) (feats ! i))
+                (indexed obs))
+           (map werr ws)
+
+-- We compute the Jacobian chunks in the following packed arrays:
+--   - 2p x 11: d(flatten(reproj_error)) / d(cams_{obs[i]})
+--   - 2p x 3: d(flatten(reproj_error)) / d(pts_{obs[i]})
+--   - 2p: d(flatten(reproj_error)) / d(w_i)
+--   - p: d(w_error) / d(w_i)
+jacobianPacked :: Acc BAIn -> Acc (Matrix FLT, Matrix FLT, Vector FLT, Vector FLT)
+jacobianPacked (BAIn cams pts ws feats obs) =
+    let I1 p = shape pts
+        grads :: Acc (Vector ((Camera, Camera), (Pt3D, Pt3D), (FLT, FLT)))
+        grads = map (\(T2 i (Observation ci pi)) ->
+                        let T4 dcam1 dpt1 dw1 _ =
+                                gradientE (\(T4 cam pt w feat) -> fst (reprojerr cam pt w feat))
+                                          (T4 (cams ! I1 ci) (pts ! I1 pi) (ws ! i) (feats ! i))
+                            T4 dcam2 dpt2 dw2 _ =
+                                gradientE (\(T4 cam pt w feat) -> snd (reprojerr cam pt w feat))
+                                          (T4 (cams ! I1 ci) (pts ! I1 pi) (ws ! i) (feats ! i))
+                        in T3 (T2 dcam1 dcam2) (T2 dpt1 dpt2) (T2 dw1 dw2))
+                    (indexed obs)
+        campacked = generate (I2 (2 * p) 11)
+                             (\(I2 i j) -> let T3 (T2 dcam1 dcam2) _ _ = grads ! I1 (i `div` 2)
+                                           in camIndex (cond (i `mod` 2 == 0) dcam1 dcam2) j)
+        ptpacked = generate (I2 (2 * p) 3)
+                            (\(I2 i j) -> let T3 _ (T2 dpt1 dpt2) _ = grads ! I1 (i `div` 2)
+                                          in ptIndex (cond (i `mod` 2 == 0) dpt1 dpt2) j)
+        rwpacked = generate (I1 (2 * p))
+                            (\(I1 i) -> let T3 _ _ (T2 dw1 dw2) = grads ! I1 (i `div` 2)
+                                        in cond (i `mod` 2 == 0) dw1 dw2)
+        wwpacked = map (gradientE werr) ws
+    in T4 campacked ptpacked rwpacked wwpacked
+
+jacobian :: Acc BAIn -> Acc BAOut
+jacobian input@(BAIn cams pts _ _ obs) =
+    let I1 n = shape cams
+        I1 m = shape pts
+        I1 p = shape obs
+
+        T4 dcams dpts drdw dwdw = jacobianPacked input
+
+        rows = enumFromStepN (I1 (2 * p)) 0 15 ++ enumFromN (I1 (p + 1)) (2 * p * 15)
+        colsvals =
+            generate (I1 (2 * p * 15))
+                     (\(I1 i) -> let obsi = i `div` (2 * 15)
+                                     valouti = i `div` 15
+                                     Observation ci pi = obs ! I1 obsi
+                                     j = i `mod` 15
+                                 in cond (j < 11)
+                                         (T2 (11 * ci + j) (dcams ! I2 valouti j))
+                                         (cond (j < 14)
+                                               (T2 (11 * n + 3 * pi + j - 11) (dpts ! I2 valouti (j - 11)))
+                                               (T2 (11 * n + 3 * m + obsi) (drdw ! I1 valouti))))
+            ++
+            generate (I1 p)
+                     (\(I1 i) -> T2 (11 * n + 3 * m + i) (dwdw ! I1 i))
+    in BAOut rows (map fst colsvals) (map snd colsvals)
+
+
+newtype ObjectiveProgram = ObjectiveProgram (BAIn -> BAFVal)
+  deriving (Generic)
+instance NFData ObjectiveProgram
+
+run1Of :: (Arrays a, Arrays b) => BackendKind -> (Acc a -> Acc b) -> a -> b
+run1Of Interpreter = I.run1
+run1Of CPU = CPU.run1
+-- run1Of GPU = GPU.run1
+
+baObjectiveProgram :: BackendKind -> ObjectiveProgram
+baObjectiveProgram kind = ObjectiveProgram (run1Of kind objective)
+
+baObjective :: ObjectiveProgram -> BAIn -> BAFVal
+baObjective (ObjectiveProgram prog) input = prog input
+
+newtype ObjectiveJacProgram = ObjectiveJacProgram (BAIn -> BAOut)
+  deriving (Generic)
+instance NFData ObjectiveJacProgram
+
+baObjectiveJacProgram :: BackendKind -> ObjectiveJacProgram
+baObjectiveJacProgram kind = ObjectiveJacProgram $ run1Of kind jacobian
+
+baObjectiveJac :: ObjectiveJacProgram -> BAIn -> BAOut
+baObjectiveJac (ObjectiveJacProgram prog) input = prog input
+
+
+class (Elt t, Elt a) => TLift t a where
+    tlift :: (Exp a -> Exp a -> Exp a) -> Exp t -> Exp t -> Exp t
+    tlift1 :: (Exp a -> Exp a -> Exp a) -> Exp a -> Exp t -> Exp t
+    treduce :: (Exp a -> Exp a -> Exp a) -> Exp t -> Exp a
+
+instance Elt a => TLift a a where
+    tlift f = f
+    tlift1 f = f
+    treduce _ x = x
+
+instance (TLift t1 a, TLift t2 a) => TLift (t1, t2) a where
+    tlift f (T2 a1 a2) (T2 b1 b2) = T2 (tlift f a1 b1) (tlift f a2 b2)
+    tlift1 f a (T2 b1 b2) = T2 (tlift1 f a b1) (tlift1 f a b2)
+    treduce f (T2 x y) = treduce f x `f` treduce f y
+
+instance (TLift t1 a, TLift t2 a, TLift t3 a) => TLift (t1, t2, t3) a where
+    tlift f (T3 a1 a2 a3) (T3 b1 b2 b3) = T3 (tlift f a1 b1) (tlift f a2 b2) (tlift f a3 b3)
+    tlift1 f a (T3 b1 b2 b3) = T3 (tlift1 f a b1) (tlift1 f a b2) (tlift1 f a b3)
+    treduce f (T3 x y z) = treduce f x `f` treduce f y `f` treduce f z
---
+++ tools/Accelerate/src/BAIO.hs
@@ -0,0 +1,150 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ViewPatterns #-}
+module BAIO (
+    Camera(..), pattern Camera,
+    Observation(..), pattern Observation,
+    BAIn(..), pattern BAIn,
+    BAFVal(..), pattern BAFVal,
+    BAOut(..), pattern BAOut,
+    Pt3D, Pt2D,
+    baReadInstance,
+    baWriteJacobian
+) where
+
+import Control.DeepSeq (NFData)
+import Data.Array.Accelerate (Elt, Arrays, Exp, Acc, Vector
+                             ,Z(..), (:.)(..), pattern Pattern, Generic)
+import qualified Data.Array.Accelerate as A
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder as BSB
+import qualified Data.ByteString.Lazy as BSL
+import Data.Double.Conversion.ByteString (toFixed)
+import System.IO (hPutStrLn, withFile, IOMode(WriteMode))
+
+import Types
+
+
+type Pt3D = (FLT, FLT, FLT)
+type Pt2D = (FLT, FLT)
+
+data Camera =
+    Camera_ { camR     :: !Pt3D
+            , camC     :: !Pt3D
+            , camF     :: !FLT
+            , camX0    :: !Pt2D
+            , camKappa :: !Pt2D }
+  deriving (Generic)
+
+deriving instance Show Camera
+instance Elt Camera
+instance NFData Camera
+
+pattern Camera :: Exp Pt3D -> Exp Pt3D -> Exp FLT -> Exp Pt2D -> Exp Pt2D -> Exp Camera
+pattern Camera r c f x0 k = Pattern (r, c, f, x0, k)
+{-# COMPLETE Camera #-}
+
+data Observation =
+    Observation_ { obsCamIdx :: !Int
+                 , obsPtIdx  :: !Int }
+  deriving (Generic)
+
+deriving instance Show Observation
+instance Elt Observation
+instance NFData Observation
+
+pattern Observation :: Exp Int -> Exp Int -> Exp Observation
+pattern Observation ci pi = Pattern (ci, pi)
+{-# COMPLETE Observation #-}
+
+data BAIn =
+    BAIn_ { baInCams  :: !(Vector Camera)        -- [n]
+          , baInPts   :: !(Vector Pt3D)          -- [m]
+          , baInW     :: !(Vector FLT)           -- [p]
+          , baInFeats :: !(Vector Pt2D)          -- [p]
+          , baInObs   :: !(Vector Observation) } -- [p]
+  deriving (Generic)
+
+deriving instance Show BAIn
+instance Arrays BAIn
+instance NFData BAIn
+
+pattern BAIn :: Acc (Vector Camera) -> Acc (Vector Pt3D) -> Acc (Vector FLT) -> Acc (Vector Pt2D) -> Acc (Vector Observation) -> Acc BAIn
+pattern BAIn c p w f o = Pattern (c, p, w, f, o)
+{-# COMPLETE BAIn #-}
+
+data BAFVal =
+    BAFVal_ { baFValReprojErr :: !(Vector Pt2D)  -- [p]
+            , baFValWErr      :: !(Vector FLT) } -- [p]
+  deriving (Generic)
+
+deriving instance Show BAFVal
+instance Arrays BAFVal
+instance NFData BAFVal
+
+pattern BAFVal :: Acc (Vector Pt2D) -> Acc (Vector FLT) -> Acc BAFVal
+pattern BAFVal r w = Pattern (r, w)
+{-# COMPLETE BAFVal #-}
+
+data BAOut =
+    BAOut_ { baOutRows :: !(Vector Int)   -- [2p + p]
+           , baOutCols :: !(Vector Int)   -- [11n + 3m + p]
+           , baOutVals :: !(Vector FLT) } -- [31p]
+  deriving (Generic)
+
+deriving instance Show BAOut
+instance Arrays BAOut
+instance NFData BAOut
+
+pattern BAOut :: Acc (Vector Int) -> Acc (Vector Int) -> Acc (Vector FLT) -> Acc BAOut
+pattern BAOut r c v = Pattern (r, c, v)
+{-# COMPLETE BAOut #-}
+
+baReadInstance :: FilePath -> IO BAIn
+baReadInstance fpath = do
+    terms <- words <$> readFile fpath
+    let slice i cnt = take cnt (drop i terms)
+        [read -> n, read -> m, read -> p] = slice 0 3
+        [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11] = map read (slice 3 11)
+        cam = Camera_ (c1, c2, c3) (c4, c5, c6) c7 (c8, c9) (c10, c11)
+        [p1, p2, p3] = map read (slice 14 3)
+        pt = (p1, p2, p3)
+        w = read (terms !! 17)
+        [f1, f2] = map read (slice 18 2)
+        feat = (f1, f2)
+        reparr :: Elt a => Int -> a -> Vector a
+        reparr cnt x = A.fromFunction (Z :. cnt) (const x)
+    return $ BAIn_ { baInCams  = reparr n cam
+                   , baInPts   = reparr m pt
+                   , baInW     = reparr p w
+                   , baInFeats = reparr p feat
+                   , baInObs   = A.fromFunction (Z :. p) $
+                                    \(Z :. i) -> Observation_ (i `rem` n) (i `rem` m)
+                   }
+
+baWriteJacobian :: FilePath -> BAIn -> BAOut -> IO ()
+baWriteJacobian fpath input out =
+    let n = A.arraySize (baInCams input)
+        m = A.arraySize (baInPts input)
+        p = A.arraySize (baInW input)
+        nrows = 2 * p + p
+        ncols = 11 * n + 3 * m + p
+        rows = A.toList (baOutRows out)
+        cols = A.toList (baOutCols out)
+        vals = A.toList (baOutVals out)
+    in withFile fpath WriteMode $ \f -> do
+        hPutStrLn f $ unwords [show nrows, show ncols]
+
+        hPutStrLn f $ show (A.arraySize (baOutRows out))
+        BS.hPut f $ BS.intercalate (BS.pack [32]) (map (toByteString . BSB.intDec) rows)
+        hPutStrLn f ""
+
+        hPutStrLn f $ show (A.arraySize (baOutCols out))
+        BS.hPut f $ BS.intercalate (BS.pack [32]) (map (toByteString . BSB.intDec) cols)
+        hPutStrLn f ""
+
+        BS.hPut f $ BS.intercalate (BS.pack [32]) (map (toFixed 15 . realToFrac) vals)
+        hPutStrLn f ""
+  where
+    toByteString = BSL.toStrict . BSB.toLazyByteString
